7730ccd8cbdd535096417202b31f594fc4218951
w4ngzhen <w4ngzhen@hotmail.com>
2024-10-21 09:54:33 +0800

    feat: 编写perlin生成。

--- crates/map/src/map_generate.rs
+++ crates/map/src/map_generate.rs
@@ -10,13 +10,13 @@ impl MapGenerate {
         chunk_w: i32,
         chunk_h: i32,
         chunk_size: i32,
-    ) -> Vec<i32> {
-        let mut map: Vec<i32> = Vec::with_capacity(
+    ) -> Vec<f64> {
+        let mut map: Vec<f64> = Vec::with_capacity(
             (chunk_w * chunk_h * chunk_size * chunk_size) as usize,
         );
         for chunk_x in 0..chunk_w {
             for chunk_y in 0..chunk_h {
-                let list = self.build_chunk(chunk_x, chunk_y, chunk_size);
+                let list = self.build_chunk(chunk_size);
                 list.iter().for_each(|&x| {
                     map.push(x);
                 })
@@ -25,25 +25,22 @@ impl MapGenerate {
         map
     }
 
-    fn build_chunk(
-        &self,
-        chunk_x: i32,
-        chunk_y: i32,
-        chunk_size: i32,
-    ) -> Vec<i32> {
+    fn build_chunk(&self, chunk_size: i32) -> Vec<f64> {
         let mut list: Vec<i32> =
             Vec::with_capacity((chunk_size * chunk_size) as usize);
         // 这里我们暂时先固定4个
-        let g00 = (-1, 1);
-        let g01 = (0, 1);
-        let g10 = (-1, -1);
-        let g11 = (-1, 1);
-        let c00 = (chunk_x, chunk_y);
-        let c01 = (chunk_x + chunk_size, chunk_y);
-        let c10 = (chunk_x + chunk_size, chunk_y + chunk_size);
-        let c11 = (chunk_x, chunk_y + chunk_size);
-        for x in chunk_x..chunk_x + chunk_size {
-            for y in chunk_y..chunk_y + chunk_size {
+        let g00 = (-2, 1);
+        let g01 = (3, 1);
+        let g10 = (-3, -1);
+        let g11 = (-1, 2);
+        let c00 = (0, 0);
+        let c01 = (chunk_size, 0);
+        let c10 = (chunk_size, chunk_size);
+        let c11 = (0, chunk_size);
+        let mut min = i32::MAX;
+        let mut max = i32::MIN;
+        for x in 0..chunk_size {
+            for y in 0..chunk_size {
                 let v00 = (x - c00.0, y - c00.1);
                 let v01 = (x - c01.0, y - c01.1);
                 let v10 = (x - c10.0, y - c10.1);
@@ -52,10 +49,23 @@ impl MapGenerate {
                 let val01 = v01.0 * g01.0 + v01.1 * g01.1;
                 let val10 = v10.0 * g10.0 + v10.1 * g10.1;
                 let val11 = v11.0 * g11.0 + v11.1 * g11.1;
+                let val = val00 + val01 + val10 + val11;
+                if val < min {
+                    min = val;
+                } else if val > max {
+                    max = val;
+                }
                 list.push(val00 + val01 + val10 + val11);
             }
         }
-        list
+        // 归一化
+        if min < 0 {
+            list = list.iter().map(|x| *x + min.abs()).collect();
+            max += min.abs();
+        }
+        let result: Vec<f64> =
+            list.iter().map(|x| *x as f64 / max as f64).collect();
+        result
     }
 }
 #[cfg(test)]
@@ -67,9 +77,10 @@ mod tests {
     #[test]
     fn test() {
         let generate = MapGenerate::new();
-        let result = generate.build(2, 2, 16);
+        let result = generate.build(1, 1, 1024);
         println!("{:?}", result);
-        let pixels = result.iter().map(|x| *x as u8).collect::<Vec<_>>();
+        let pixels =
+            result.iter().map(|x| (*x * 255.) as u8).collect::<Vec<_>>();
         let target = Path::new("example_images/").join(Path::new("map.png"));
         fs::create_dir_all(
             target.clone().parent().expect("No parent directory found."),
@@ -79,8 +90,8 @@ mod tests {
         let _ = image::save_buffer(
             target,
             &pixels,
-            2 * 16,
-            2 * 16,
+            1 * 1024,
+            1 * 1024,
             image::ColorType::L8,
         );
     }
